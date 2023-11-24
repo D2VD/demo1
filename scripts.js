1845d8d81a3dc679394f0f1c1a51bbe5b5438535
D2VD <80351191+D2VD@users.noreply.github.com>
2023-11-25 00:11:05 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,41 +1,13 @@
-// Get the navigation menu
-const navigation = document.querySelector("nav");
+/* =========== Toggle Menu ============ */
+const openNav = document.querySelector(".open-btn");
+const closeNav = document.querySelector(".close-btn");
+const menu = document.querySelector(".nav-list");
 
-// Add a toggle button to the navigation menu
-const toggleButton = document.createElement("button");
-toggleButton.innerHTML = "Toggle Navigation";
-toggleButton.style.backgroundColor = "darkgray";
-toggleButton.style.color = "white";
-toggleButton.style.padding = "10px";
-toggleButton.style.float = "right";
-toggleButton.addEventListener("click", function() {
-  navigation.classList.toggle("active");
+openNav.addEventListener("click", () => {
+  menu.classList.add("show");
 });
-navigation.appendChild(toggleButton);
 
-// Add CSS styles for the active class
-const style = document.createElement("style");
-style.innerHTML = `
-nav.active {
-  display: flex;
-  flex-direction: column;
-  position: absolute;
-  top: 60px;
-  right: 0;
-  background-color: darkgray;
-  width: 100%;
-  height: 100%;
-}
+closeNav.addEventListener("click", () => {
+  menu.classList.remove("show");
+});
 
-nav.active ul {
-  display: block;
-  text-align: center;
-}
-
-nav.active a {
-  display: block;
-  padding: 20px;
-  margin: 20px 0;
-}
-`;
-document.head.appendChild(style);
